aa05f7f2ab34aae017527857efb534365df4543a
Cristianftf <tcristianftf@gmail.com>
2025-10-13 16:12:40 -0400

    asd

--- demo/src/main/java/com/spring_boot_training/demo/repository/UserRepository.java
+++ demo/src/main/java/com/spring_boot_training/demo/repository/UserRepository.java
@@ -1,9 +1,8 @@
 package com.spring_boot_training.demo.repository;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
