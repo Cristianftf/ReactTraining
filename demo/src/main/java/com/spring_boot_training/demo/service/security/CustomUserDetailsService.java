c31dcbb523393e6955d6a0faefc7af3152b4eb50
Cristianftf <tcristianftf@gmail.com>
2025-10-14 23:42:18 -0400

    implements UserDetails

--- demo/src/main/java/com/spring_boot_training/demo/service/security/CustomUserDetailsService.java
+++ demo/src/main/java/com/spring_boot_training/demo/service/security/CustomUserDetailsService.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.spring_boot_training.demo.model.CustomUserDetails;
 import com.spring_boot_training.demo.model.User;
 import com.spring_boot_training.demo.repository.UserRepository;
 
