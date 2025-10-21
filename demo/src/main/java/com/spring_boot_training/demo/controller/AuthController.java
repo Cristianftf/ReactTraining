f00aaf0ffc214c9565b174b6d7f3f3b748306192
cristianftf <tcristianftf@gmail.com>
2025-10-21 10:23:54 +0200

    x

--- demo/src/main/java/com/spring_boot_training/demo/controller/AuthController.java
+++ demo/src/main/java/com/spring_boot_training/demo/controller/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
  
 
 @RestController
-@RequestMapping("/apiu/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
